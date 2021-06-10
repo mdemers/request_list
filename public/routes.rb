3fa5c718429efea57f1dd4d3c1c29f8fcf7cd3ce
James Bullen <jambun@gmail.com>
2021-06-10 11:19:20 +1000

    change endpoint to sidestep the greedy slugs route

--- public/routes.rb
+++ public/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get  '/plugin/request_list',       to: 'request_list#index'
+  get  '/plugin/request_list/list',  to: 'request_list#index'
   post '/plugin/request_list/email', to: 'request_list#email'
   get  '/plugin/request_list/pdf',   to: 'request_list#pdf'
 
