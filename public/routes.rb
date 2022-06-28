cf5ecc7eb3982c425b4bbd27e48178408386ed6b
James Bullen <jambun@gmail.com>
2022-06-28 17:05:54 +1000

    make routes prefix aware

--- public/routes.rb
+++ public/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-
-  get  '/plugin/request_list/list',  to: 'request_list#index'
-  post '/plugin/request_list/email', to: 'request_list#email'
-  get  '/plugin/request_list/pdf',   to: 'request_list#pdf'
-
+  [AppConfig[:public_proxy_prefix], AppConfig[:public_prefix]].uniq.each do |prefix|
+    scope prefix do
+      get  '/plugin/request_list/list',  to: 'request_list#index'
+      post '/plugin/request_list/email', to: 'request_list#email'
+      get  '/plugin/request_list/pdf',   to: 'request_list#pdf'
+    end
+  end
 end
